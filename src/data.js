c42256624aa9899178184af5c682feb890fbbd4c
omerkr0 <omerkaramanx@gmail.com>
2023-12-18 22:15:20 +0300

    fixed image paths in projects data file

--- src/data.js
+++ src/data.js
@@ -1,10 +1,14 @@
+import starwars from "../public/assets/star_wars.png"
+import book from "../public/assets/book.png"
+
+
 export const projectsData = [
   {
     id: 1,
     name: 'Star Wars App',
     description:
       ' The app allows you to search for spaceships by name and model, and on the detail page you can also access detailed information about these spaceships',
-    image: 'public/assets/star_wars.png',
+    image: {starwars},
     tech: 'React, React Bootstrap, React Router, Framer Motion',
     github: 'https://github.com/omerkr0/star-wars-app',
     live: 'https://star-wars-starships-app.netlify.app/',
@@ -14,7 +18,7 @@ export const projectsData = [
     name: 'Book App',
     description:
       ' The Book App allows you to search for books by title, author, genre and other criteria, as well as access detailed information about those books on the detail page.',
-    image: 'public/assets/book.png',
+    image: {book},
     tech: 'React, React Router, CSS',
     github: 'https://github.com/omerkr0/book-app',
     live: 'https://book-app-omerkr0.vercel.app/',
@@ -23,7 +27,7 @@ export const projectsData = [
     id: 3,
     name: 'Tic Tac Toe Game',
     description: "The Book App allows you to search for books by title, author, genre and other criteria, as well as access detailed information about those books on the detail page",
-    image: "public/assets/book.png",
+    image: {book},
     tech: 'React, React Router, CSS',
     github: 'https://github.com/omerkr0/book-app',
     live: 'https://book-app-omerkr0.vercel.app/',
@@ -32,7 +36,7 @@ export const projectsData = [
     id: 4,
     name: 'Tic Tac Toe Game',
     description: "The Book App allows you to search for books by title, author, genre and other criteria, as well as access detailed information about those books on the detail page",
-    image: "public/assets/book.png",
+    image: {book},
     tech: 'React, React Router, CSS',
     github: 'https://github.com/omerkr0/book-app',
     live: 'https://book-app-omerkr0.vercel.app/',
