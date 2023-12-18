eb4aa25ee3d75c1f7dd823e5cceeec8d4ee8cdc9
omerkr0 <omerkaramanx@gmail.com>
2023-12-18 18:45:25 +0300

    projects data file added

---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const projectsData = [
+  {
+    id: 1,
+    name: 'Star Wars App',
+    description:
+      ' The app allows you to search for spaceships by name and model, and on the detail page you can also access detailed information about these spaceships',
+    image: 'src/assets/star_wars.png',
+    tech: 'React, React Bootstrap, React Router, Framer Motion',
+    github: 'https://github.com/omerkr0/star-wars-app',
+    live: 'https://star-wars-starships-app.netlify.app/',
+  },
+  {
+    id: 2,
+    name: 'Book App',
+    description:
+      ' The Book App allows you to search for books by title, author, genre and other criteria, as well as access detailed information about those books on the detail page.',
+    image: 'src/assets/book.png',
+    tech: 'React, React Router, CSS',
+    github: 'https://github.com/omerkr0/book-app',
+    live: 'https://book-app-omerkr0.vercel.app/',
+  },
+  {
+    id: 3,
+    name: 'Tic Tac Toe Game',
+    description: "The Book App allows you to search for books by title, author, genre and other criteria, as well as access detailed information about those books on the detail page",
+    image: "src/assets/book.png",
+    tech: 'React, React Router, CSS',
+    github: 'https://github.com/omerkr0/book-app',
+    live: 'https://book-app-omerkr0.vercel.app/',
+  },
+  {
+    id: 4,
+    name: 'Tic Tac Toe Game',
+    description: "The Book App allows you to search for books by title, author, genre and other criteria, as well as access detailed information about those books on the detail page",
+    image: "src/assets/book.png",
+    tech: 'React, React Router, CSS',
+    github: 'https://github.com/omerkr0/book-app',
+    live: 'https://book-app-omerkr0.vercel.app/',
+  }
+]
