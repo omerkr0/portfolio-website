d3999960dba8edb12aa053fb8f4af4b6fe0a8890
omerkr0 <omerkaramanx@gmail.com>
2023-12-18 18:23:10 +0300

    footer component added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="container mx-auto text-center">
+        <p>
+          &copy; {new Date().getFullYear()} Ömer Karaman Tüm hakları saklıdır.
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
